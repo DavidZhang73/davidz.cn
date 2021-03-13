bed1ca0cbed1bd34217c9f4d95523a03027fd74a
DavidZ <davidbill@outlook.com>
2021-03-13 16:30:51 +0800

    :truck: update icons

--- src/sites.js
+++ src/sites.js
@@ -3,84 +3,84 @@ export default [
     id: 'blog',
     title: 'Blog',
     url: 'https://blog.davidz.cn/',
-    logo: 'https://davidz.cn/img/logo_icon.png',
+    logo: '/img/logo.svg',
     description: '个人博客'
   },
   {
     id: 'academic',
     title: 'Academic',
     url: 'https://academic.davidz.cn/',
-    logo: 'https://davidz.cn/img/academic.svg',
+    logo: '/img/academic.png',
     description: '个人主页'
   },
   {
     id: 'vidat',
     title: 'Vidat',
     url: 'https://vidat.davidz.cn/',
-    logo: 'https://davidz.cn/img/vidat.svg',
+    logo: '/img/vidat.svg',
     description: '视频标注'
   },
   {
     id: 'papereader',
     title: 'Papereader',
     url: 'https://papereader.davidz.cn/',
-    logo: 'https://davidz.cn/img/logo_icon.png',
+    logo: '/img/logo.svg',
     description: '阅读论文'
   },
   {
     id: 'daty',
     title: 'Daty',
     url: 'https://daty.davidz.cn/',
-    logo: 'https://davidz.cn/img/daty.png',
+    logo: '/img/daty.png',
     description: '文件收集'
   },
   {
     id: 'git',
     title: 'Git',
     url: 'https://git.davidz.cn/',
-    logo: 'https://davidz.cn/img/gitea.png',
+    logo: '/img/gitea.svg',
     description: '代码托管'
   },
   {
     id: 'file',
     title: 'FileBrowser',
     url: 'https://file.davidz.cn/',
-    logo: 'https://davidz.cn/img/filebrowser.svg',
+    logo: '/img/filebrowser.svg',
     description: '私有云'
   },
   {
     id: 'kms',
     title: 'KMS',
     url: 'https://kms.davidz.cn/',
-    logo: 'https://davidz.cn/img/kms.png',
+    logo: '/img/kms.svg',
     description: '激活服务'
   },
   {
     id: 'jenkins-update',
     title: 'Jenkins',
     url: 'https://jenkins-update.davidz.cn/',
-    logo: 'https://davidz.cn/img/jenkins-update.svg',
+    logo: '/img/jenkins-update.svg',
     description: '更新中心'
   },
   {
     id: 'rabbitmq',
     title: 'RabbitMQ',
     url: 'https://rabbitmq.davidz.cn/',
-    logo: 'https://davidz.cn/img/rabbitmq.svg',
+    logo: '/img/rabbitmq.svg',
     description: 'RabbitMQ 管理'
   },
   {
     id: 'docker',
     title: 'Docker',
     url: 'https://docker.davidz.cn/',
-    logo: 'https://davidz.cn/img/docker.png',
+    logo: '/img/docker.svg',
     description: 'Docker 管理'
   },
   {
     id: 'netdata',
     title: 'Netdata',
     url: 'https://netdata.davidz.cn/',
-    logo: 'https://davidz.cn/img/netdata.svg',
+    logo: '/img/netdata.svg',
     description: '服务器状态'
   }
 ]
